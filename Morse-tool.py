530ebb775544cd4ff6fb69c76e4e9bf5931109fa
StephaneBranly <stephanebranly.pro@gmail.com>
2020-04-09 22:37:09 +0200

    add: encode and decode functions

--- Morse-tool.py
+++ Morse-tool.py
@@ -1,50 +1,68 @@
+import numpy as np
+ 
 continuer = True
-code = [['a','.-'],
-['b','-...'],
-['c','-.-.'],
-['d','-..'],
-['e','.'],
-['f','..-.'],
-['g','--.'],
-['h','....'],
-['i','..'],
-['j','.---'],
-['k','-.-'],
-['l','.-..'],
-['m','--'],
-['n','-.'],
-['o','---'],
-['p','.--.'],
-['q','--.-'],
-['r','.-.'],
-['s','...'],
-['t','-'],
-['u','..-'],]
-['v','...-'],
-['w','.--'],
-['x','-..-'],
-['y','-.--'],
-['z','--..']]
+code = np.array([["a", ".-"],
+["b", "-..."],
+["c", "-.-."],
+["d", "-.."],
+["e", "."],
+["f", "..-."],
+["g", "--."],
+["h", "...."],
+["i", ".."],
+["j", ".---"],
+["k", "-.-"],
+["l", ".-.."],
+["m", "--"],
+["n", "-."],
+["o", "---"],
+["p", ".--."],
+["q", "--.-"],
+["r", ".-."],
+["s", "..."],
+["t", "-"],
+["u", "..-"],
+["v", "...-"],
+["w", ".--"],
+["x", "-..-"],
+["y", "-.--"],
+["z", "--.."]])
+
 
 def decode(sentence):
-    print("sentence = "+str(sentence))
-
-
+    final_sentence = ""
+    code_copy = []
+    code_copy = code[:,1]
+    for letter in sentence:
+        result = np.where(code_copy == letter)
+        if(len(result[0])==1):
+            final_sentence += str(code[result[0][0]][0])
+        else:
+            final_sentence += " "
+    print(str(final_sentence))
+    
 def encode(sentence):
-    print("sentence = "+str(sentence))
+    final_sentence = ""
+    for word in sentence:
+        for letter in word:
+            if(ord(letter)-97 >= 0 and ord(letter)-97 <= 25):
+                final_sentence += str(code[ord(letter)-97][1])+" "
+        final_sentence += "   "
+    print(str(final_sentence))
+
 
 
 def action(line):
     global continuer
-    line = line.split(' ')
+    line=line.split(" ")
     if(len(line) > 0):
         if(line[0] == "stop"):
-            continuer = False
+            continuer=False
         elif(line[0] == "decode" and len(line) > 1):
             decode(line[1:])
         elif(line[0] == "encode" and len(line) > 1):
             encode(line[1:])
-
+    print()
 
 print("\033[0;37;41m#####      Morse-Tool      #####")
 print("")
@@ -56,5 +74,5 @@ print("\033[0;37;41m#####                       #####")
 print("\033[0;37;48m")
 
 while(continuer):
-    line = input("> ")
+    line=input("> ")
     action(line)
